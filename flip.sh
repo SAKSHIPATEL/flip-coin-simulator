e41f4b42baaa8b0beb3b0d846cdb6aeeb8dc38aa
SAKSHIPATEL <patelsakshi121@gmail.com>
2020-10-10 08:51:33 +0530

    Added : Random value to generate head and tail

--- flip.sh
+++ flip.sh
@@ -1,1 +1,13 @@
-echo "Flip Coin Simulator"
+#!/bin/bash -x
+
+for((i=0;i<2;i++))
+do
+	randomCheck=$((RANDOM%2))
+
+done
+if [ $randomCheck -eq 0 ]
+then
+	echo "flipped coin is head"
+else
+	echo "flipped coin is tail"
+fi
